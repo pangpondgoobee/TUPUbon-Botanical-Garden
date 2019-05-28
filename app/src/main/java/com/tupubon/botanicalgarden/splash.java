bfec54838d0b5e9bdb316a8fb6fd9d4cdb479e8b
pangpondgoobee <pangpondgoobee@hotmail.com>
2019-05-28 12:21:48 +0700

    Initial commit

---
+++ app/src/main/java/com/tupubon/botanicalgarden/splash.java
@@ -0,0 +1,29 @@
+package com.tupubon.botanicalgarden;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class splash extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash.this, home.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+}
